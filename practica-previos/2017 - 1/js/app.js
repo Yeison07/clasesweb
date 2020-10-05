f39d2d742334f898f38b93619c9596b3be429c57
yeison07 <yeisonalexanderss@ufps.edu.co>
2020-10-05 14:58:29 -0500

    archivos

---
+++ practica-previos/2017 - 1/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$('#btn_iniciar').click(function(){
+$('#modal_tab').modal();
+});
+
+$('#btn_registrar').click(function(){
+$('#modal_tab2').modal();
+});
+
+
+});
